c970df30586e81facf88ae8593cc0a7e5cb49b06
Henrique Milioli Ronchi <henrique.ronchi>
2026-09-22 19:25:14 -0300

    Initial Commit

---
+++ src/main/java/com/example/SportFlow/SportFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportFlowApplication.class, args);
+	}
+
+}
